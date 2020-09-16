f3df67e3b5f59657513df2d86bc7d5fa7deb8f42
Alvina <alvinamohsin5@gmail.com>
2020-09-16 14:39:06 +0500

    added toggle function

--- src/js/TodoController.js
+++ src/js/TodoController.js
@@ -1,34 +1,48 @@
-import TodoModel from './TodoModel.js';
+import TodoModel from "./TodoModel.js";
 
 export default function TodoListController() {
-    this.todoList = [];
-    this.renderList = function () {
-        document.querySelector(".list").innerHTML = this.todoList.map((item) => {
-            return `
+  this.todoList = [];
+
+  // this.toggleCheck = function test() {
+  //   console.log("item");
+  // };
+
+  this.renderList = function () {
+    document.querySelector(".list").innerHTML = this.todoList
+      .map((item) => {
+        return `
             <li>
-                <input type="checkbox" id=${item.id} class="checkbox" />
+                <input type="checkbox" id=${
+                  item.id
+                } class="checkbox" onclick="${() => console.log("this")}" />
                 <label for=${item.id}>${item.text}</label>
                 <span></span>
             </li>
-            `
-        }).join("");
-    }
+            `;
+      })
+      .join("");
+  };
 
-    this.addTodo = function (text) {
-        const todoListItem = new TodoModel(text);
-        this.todoList.push(todoListItem);
-        this.renderList();
+  this.addTodo = function (text) {
+    const todoListItem = new TodoModel(text);
+    this.todoList.push(todoListItem);
+    console.log(this.todoList);
+    this.renderList();
+  };
 
-    }
+  this.checkForInput = () => {
+    const inputElement = document.querySelector(".todo-text");
+    inputElement.addEventListener("keyup", (event) => {
+      if (event.keyCode === 13 && event.target.value !== "") {
+        const value = event.target.value;
+        this.addTodo(value);
+      }
+    });
+  };
 
-    this.checkForInput = () => {
-        const inputElement = document.querySelector('.todo-text');
-        inputElement.addEventListener('keyup', (event) => {
-            if (event.keyCode === 13 && event.target.value !== '') {
-                const value = event.target.value;
-                this.addTodo(value);
-            }
-        })
-    }
-
-};
+  //   this.checkCompleted = () => {
+  //       this.todoList.filter(listItem = > {
+  //           if()
+  //       })
+  //   }
+}
